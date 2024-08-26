68904c504fb2bd9d194e071c6259f36f5d518528
edellveis <161501144+edellveis@users.noreply.github.com>
2024-08-26 14:50:02 +0300

    dz-12-js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 axios.defaults.baseURL = 'https://pixabay.com/api/'
 
-export function getImages(imgType) {
+export function getImages(inputValue, page) {
     const axiosOptions = {
         params: {
             key: '45416284-694200f21ad4df97e05eb7dcc',
-            q: imgType,
+            page:`${page}`,
+            per_page: 15,
+            q: inputValue,
             image_type: 'photo', 
             orientation: 'horizontal', 
             safesearch: true,
         }}
     return axios.get('', axiosOptions)
     };
-    
+  
--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 
-import { getImages } from "./js/pixabay-api";
+import { getImages} from "./js/pixabay-api";
 import { createImageCard } from "./js/render-functions";
 
 
@@ -23,29 +23,80 @@ const refs = {
     form: document.querySelector('form'),
     gallery: document.querySelector('.gallery'),
     loader: document.querySelector('.loader'),
+    btnLoadMore: document.querySelector('.btn-load-more')
 };
+let currnetPage = 1;
+let inputValue = ''
 
-refs.form.addEventListener('submit',async (event) => {
-    try {
-      event.preventDefault();
-    const inputValue = refs.form.elements.user_select.value;
-    refs.gallery.innerHTML = ''; 
-    refs.loader.style.display = 'block';
-      const  response =  await getImages(inputValue);
-      if (response.data.hits.length === 0) {
-            iziToast.show({
-              title: 'Sorry, there are no images matching your search query. Please try again!',
-              color: 'red',
-              position: 'topRight',
-              progressBar: false,
-            });
-          }
-            const imgCards = response.data.hits.map(createImageCard).join('');
-            refs.gallery.innerHTML = imgCards;
-            lightbox.refresh();      
-    } catch (err) {
-      console.log(err);
-    } finally{
-      refs.loader.style.display = 'none';
-    }})
-    // axios te
+async function searchSubmit (event) {
+  try {
+    event.preventDefault();
+   inputValue = refs.form.elements.user_select.value;
+
+   currnetPage = 1;
+
+
+   refs.gallery.innerHTML = ''; 
+   refs.loader.style.display = 'block';
+    const  response =  await getImages(inputValue,currnetPage);
+    if (response.data.hits.length === 0) {
+          iziToast.show({
+            title: 'Sorry, there are no images matching your search query. Please try again!',
+            color: 'red',
+            position: 'topRight',
+            progressBar: false,
+          });
+        }
+          const imgCards = response.data.hits.map(createImageCard).join('');
+          refs.gallery.innerHTML = imgCards;
+          lightbox.refresh();
+          refs.btnLoadMore.classList.remove('is-hidden');
+  } catch (err) {
+    console.log(err);
+  } finally{
+    refs.loader.style.display = 'none';
+  }
+}
+async function  onLoadMore (event) {
+      try {
+
+        refs.btnLoadMore.classList.add('is-hidden'); 
+        refs.loader.style.display = 'block';
+
+
+          currnetPage ++;
+
+          const response = await getImages(inputValue, currnetPage);          
+          
+          const imgCards = response.data.hits.map(createImageCard).join('');
+            refs.gallery.insertAdjacentHTML('beforeend', imgCards);
+            lightbox.refresh();
+
+            refs.btnLoadMore.classList.remove('is-hidden');
+
+            const { height: cardHeight } = refs.gallery.firstElementChild.getBoundingClientRect();
+
+            window.scrollBy({
+            top: cardHeight * 2,
+            behavior: 'smooth'
+        });
+          
+            const totalPages = Math.ceil(response.data.totalHits / 20);
+            if (currnetPage >= totalPages){
+              refs.btnLoadMore.classList.add('is-hidden')
+              iziToast.show({
+                title: "We're sorry, but you've reached the end of search results.",
+                color: 'blue',
+                position: 'topRight',
+                progressBar: false,
+              })
+            }
+      } catch (err) {
+          console.log(err);
+      }finally{
+        refs.loader.style.display = 'none';
+      }
+  }
+    refs.form.addEventListener('submit', searchSubmit)
+    refs.btnLoadMore.addEventListener('click', onLoadMore)
+    
